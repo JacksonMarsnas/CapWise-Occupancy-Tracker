fb29a1383a6e976198ef7e9331c3ff35c81dd8b5
Sally Poon <sallypoon@hotmail.com>
2021-03-21 13:25:48 -0700

    Instantiated Firestore DB for user login

---
+++ scripts/firebase_api.js
@@ -0,0 +1,12 @@
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyARMMT4bDtdvX-ZhncNU2qF-JHg3WulTvw",
+            authDomain: "capwise-fac45.firebaseapp.com",
+            projectId: "capwise-fac45",
+            storageBucket: "capwise-fac45.appspot.com",
+            messagingSenderId: "942821314260",
+            appId: "1:942821314260:web:21b65c3809cf795fcbb541"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        const db = firebase.firestore();
