e4a6f186903923eca8ecc46802da7123a95e1823
Sally Poon <sallypoon@hotmail.com>
2021-04-12 00:17:00 -0700

    Edit doc comments

--- scripts/login_firebase.js
+++ scripts/login_firebase.js
@@ -22,7 +22,7 @@
                                 email: user.email                          //with authenticated user's ID (user.uid)
                             }).then(function () {
                                 console.log("New user added to firestore");
-                                window.location.assign("SetUp.html");       //re-direct to main.html after signup
+                                window.location.assign("SetUp.html");       //re-direct to setUp.html after signup if they are a FIRST TIME USER
                             })
                             .catch(function (error) {
                                 console.log("Error adding new user: " + error);
@@ -40,15 +40,12 @@
             },
             // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
             signInFlow: 'popup',
-            signInSuccessUrl: 'main.html',
+            signInSuccessUrl: 'main.html',  // re-direct returning users to main.html
             signInOptions: [
                 // Leave the lines as is for the providers you want to offer your users.
                 firebase.auth.GoogleAuthProvider.PROVIDER_ID,
                 firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-                // firebase.auth.GithubAuthProvider.PROVIDER_ID,
                 firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                // firebase.auth.PhoneAuthProvider.PROVIDER_ID
             ],
             // Terms of service url.
             tosUrl: '<your-tos-url>',
